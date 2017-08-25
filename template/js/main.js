4cbf76b988fc961b39088cf457d62c44828dc58e
William I. Olojede <ilerioluwase97@gmail.com>
2017-08-25 07:07:29 +0100

    add recipe details page template

--- template/js/main.js
+++ template/js/main.js
@@ -1,3 +1,27 @@
 $(document).ready(function(){
+  // Activate Sidebar
   $(".button-collapse").sideNav();
+
+  // Smooth scrolling for write review link
+  function smoothScroll(){
+    $("a").on('click', function(event) {
+      if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+
+        // Store hash
+        var hash = this.hash;
+
+        // Using jQuery's animate() method to add smooth page scroll
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+          $("textarea")[0].focus();
+        });
+      }
+    });
+  }
+  smoothScroll();
+
+
 })
