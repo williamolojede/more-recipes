b63b5600e06b6887e5644624ce9658c21c0b89db
William I. Olojede <ilerioluwase97@gmail.com>
2017-08-31 09:26:33 +0100

    init

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import signup from './signup';
 import login from './login';
+import login from './login';
 
 const v1 = express.Router();
 
-v1.use('/v1/signup', signup);
-v1.use('/v1/login', login);
+v1.use('/v1/user/signup', signup);
+v1.use('/v1/user/login', login);
+v1.use('/v1/recipe', recipe);
 
 export default v1;
---
+++ server/routes/recipe.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import jwt from 'jsonwebtoken';
+import { Recipe } from '../models/index';
+
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+
+});
+router.post('/', (req, res, next) => {
+
+});
+router.put('/', (req, res, next) => {
+
+});
+
+router.delete('/', (req, res, next) => {
+
+});
+
+export default router;
