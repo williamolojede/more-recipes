084a1146d2469795e3e257ed54620443e910389d
William I. Olojede <ilerioluwase97@gmail.com>
2017-09-02 13:07:06 +0100

    add route to allow user modify recipe

--- server/controllers/recipes/updateRecipe.js
+++ server/controllers/recipes/updateRecipe.js
@@ -1,7 +1,63 @@
 import { Recipe } from '../../models/index';
 
 const updateRecipe = (req, res, next) => {
+  const { userID } = req;
+  const { update } = req.body;
 
+  const isEmpty = (obj) => {
+    const keys = Object.keys(obj);
+    if (keys.length === 0) return true;
+    return false;
+  };
+
+  const hasInvalidKeyName = (right, wrong) => {
+    // get keys
+    const rightKeys = Object.keys(right.dataValues);
+    const wrongKeys = Object.keys(wrong);
+    // filter for invalid key(s)
+    const invalid = wrongKeys.filter(el => !rightKeys.includes(el));
+    if (invalid.length === 0) return false;
+    return true;
+  };
+
+  if (!update || isEmpty(update) === true) {
+    const err = new Error('update property required or can not be empty');
+    err.status = 400;
+    return next(err);
+  }
+  Recipe.findById(req.params.id)
+    .then((oldRecipe) => {
+      // check if recipe exist
+      if (!oldRecipe) {
+        const err = new Error('Recipe not found');
+        err.status = 404;
+        return next(err);
+      }
+      // check if isOwner
+      if (userID !== oldRecipe.owner) {
+        const err = new Error('Not authorized to modify this recipe');
+        err.status = 403;
+        return next(err);
+      }
+      // check for invalid property name on update object
+      if (hasInvalidKeyName(oldRecipe, req.body.update)) {
+        const err = new Error('invalid property name(s) on update object');
+        err.status = 400;
+        return next(err);
+      }
+      oldRecipe.update(req.body.update)
+        .then(newRecipe => res.status(200).send({ message: 'success', recipe: newRecipe }))
+        .catch((error) => {
+          const err = new Error(error);
+          err.status = 500;
+          next(err);
+        });
+    })
+    .catch((error) => {
+      const err = new Error(error);
+      err.status = 500;
+      next(err);
+    });
 };
 
 export default updateRecipe;
--- server/middleware/isOwner.js
+++ server/middleware/isOwner.js
@@ -1,23 +1,22 @@
-import { Recipe } from '../../models/index';
+import { Recipe } from '../models';
 
-const deleteRecipe = (req, res, next) => {
+const isOwner = (req, res, next) => {
   const { userID } = req;
-  // const decoded = jwt.decode(token);
+
   Recipe.findById(req.params.id)
     .then((recipe) => {
+      // check if recipe exist
       if (!recipe) {
         const err = new Error('Recipe not found');
         err.status = 404;
         return next(err);
       }
+      // check if isOwner
       if (userID !== recipe.owner) {
         const err = new Error('Not authorized to delete this recipe');
         err.status = 403;
         return next(err);
       }
-      recipe.destroy()
-        .then(() => res.status(200).send({ message: 'success' }))
-        .catch(err => console.error(err));
     })
     .catch((error) => {
       const err = new Error(error.errors[0].message);
@@ -26,4 +25,4 @@ const deleteRecipe = (req, res, next) => {
     });
 };
 
-export default deleteRecipe;
+export default isOwner;
