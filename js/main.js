d97d4b2f5538a6649290df7f2c4d96369141425e
William I. Olojede <ilerioluwase97@gmail.com>
2017-09-08 16:40:54 +0100

    WIP

--- js/main.js
+++ js/main.js
@@ -3,25 +3,25 @@ $(document).ready(function(){
   $(".button-collapse").sideNav();
 
   // Smooth scrolling for write review link
-  function smoothScroll(){
-    $("a").on('click', function(event) {
-      if (this.hash !== "") {
-        // Prevent default anchor click behavior
-        event.preventDefault();
+  // function smoothScroll(){
+  //   $("a").on('click', function(event) {
+  //     if (this.hash !== "") {
+  //       // Prevent default anchor click behavior
+  //       event.preventDefault();
 
-        // Store hash
-        var hash = this.hash;
+  //       // Store hash
+  //       var hash = this.hash;
 
-        // Using jQuery's animate() method to add smooth page scroll
-        $('html, body').animate({
-          scrollTop: $(hash).offset().top
-        }, 800, function(){
-          $("textarea")[0].focus();
-        });
-      }
-    });
-  }
-  smoothScroll();
+  //       // Using jQuery's animate() method to add smooth page scroll
+  //       $('html, body').animate({
+  //         scrollTop: $(hash).offset().top
+  //       }, 800, function(){
+  //         $("textarea")[0].focus();
+  //       });
+  //     }
+  //   });
+  // }
+  // smoothScroll();
 
 
 })
