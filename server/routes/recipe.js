d04f83362e91489b4e2efc21c29a518d69b9af9e
William I. Olojede <ilerioluwase97@gmail.com>
2017-09-16 20:22:19 +0100

    refactor middleware import

--- server/routes/recipe.js
+++ server/routes/recipe.js
@@ -1,11 +1,5 @@
 import express from 'express';
 import recipesController from '../controllers/recipes';
-import requiresToken from '../middlewares/requiresToken';
-import validateAddRecipe from '../middlewares/validateAddRecipe';
-import doesRecipeExist from '../middlewares/doesRecipeExist';
-import ownerNotAllowed from '../middlewares/ownerNotAllowed';
-import countVotes from '../middlewares/countVotes';
-import countFavorites from '../middlewares/countFavorites';
 import middlewares from '../middlewares';
 
 // could be used on put and delete but the error won't be specific
@@ -14,13 +8,13 @@ import middlewares from '../middlewares';
 const router = express.Router();
 
 // all recipe routes requires user to have a token
-router.use('*', requiresToken);
+router.use('*', middlewares.requiresToken);
 
 router.get('/', recipesController.getAllRecipe);
-router.post('/', validateAddRecipe, recipesController.createRecipe);
+router.post('/', middlewares.validateAddRecipe, recipesController.createRecipe);
 
 // checks if the recipe to be accessed exist
-router.use('/:id', doesRecipeExist);
+router.use('/:id', middlewares.doesRecipeExist);
 
 router.route('/:id')
   .get(middlewares.countViews, recipesController.getSingleRecipe)
@@ -30,8 +24,17 @@ router.route('/:id')
 // checks if the user is owner is allowed to perform action on recipe
 // router.use('/:id/*', ownerNotAllowed);
 
-router.post('/:id/vote-:dir', recipesController.voteRecipe, countVotes);
-router.post('/:id/favorite', ownerNotAllowed, recipesController.favoriteRecipe, countFavorites);
+router.post('/:id/vote-:dir',
+  recipesController.voteRecipe,
+  middlewares.countVotes
+);
+
+router.post('/:id/favorite',
+  middlewares.ownerNotAllowed,
+  recipesController.favoriteRecipe,
+  middlewares.countFavorites
+);
+
 router.post('/:id/reviews', recipesController.reviewRecipe);
 
 export default router;
