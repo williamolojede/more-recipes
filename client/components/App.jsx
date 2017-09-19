68d80f64834a6595b7a7585370c493539e77e9d4
William I. Olojede <ilerioluwase97@gmail.com>
2017-09-19 22:41:39 +0100

    setup express serve to serve static files

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -7,26 +7,7 @@ class App extends React.Component {
   render() {
     return (
       <div className='page page__home'>
-        <header class="site-header">
-          <nav>
-            <div class="nav-wrapper">
-              <a href="/" class="brand-logo">MoreRecipes</a>
-              <ul class="right hide-on-med-and-down">
-                <li><a href="my_recipes.html">My Recipes</a></li>
-                <li><a href="login.html">Login</a></li>
-                <li><a href="register.html">Register</a></li>
-              </ul>
-              <ul class="side-nav" id="mobile-nav">
-                <li><a href="my_recipes.html">My Recipes</a></li>
-                <li><a href="login.html">Login</a></li>
-                <li><a href="register.html">Register</a></li>
-              </ul>
-              <a href="#" data-activates="mobile-nav" class="button-collapse">
-                <i class="material-icons">menu</i>
-              </a>
-            </div>
-          </nav>
-        </header>
+        <h1 style={{textAlign: 'center'}}>Something Awesome is coming 😎🔥</h1>
       </div>
     );
   }
