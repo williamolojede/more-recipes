7958dd492c9d4b66553ae6af375312b0b03bb898
William I. Olojede <ilerioluwase97@gmail.com>
2017-11-15 01:16:31 +0100

    bug(userImg) - fix code breaking when user fullname is just one word

---
+++ client/tests/components/shared/userImg.spec.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import UserImg from '../../../components/shared/UserImg';
+import user from '../../../__mocks__/user.mock';
+
+function setup() {
+  const props = {
+    user,
+    type: 'inReview'
+  };
+  const wrapper = shallow(<UserImg {...props} />);
+
+  return {
+    wrapper,
+    props
+  };
+}
+
+describe('Top Rated Recipes List', () => {
+  it('should render with right amount of elements', () => {
+    const { wrapper } = setup();
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should render properly if user fullname is just one word', () => {
+    const { wrapper, props } = setup();
+    const propsWithUserImgUrl = Object.assign({}, props);
+    propsWithUserImgUrl.user.fullname = 'OneWord';
+    wrapper.setProps({ propsWithUserImgUrl });
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should render an img tag if user imgurl is present', () => {
+    const { wrapper, props } = setup();
+    const propsWithUserImgUrl = Object.assign({}, props);
+    propsWithUserImgUrl.user.imgUrl = 'https://www.worldcrunch.com/assets/img/avatars/thumbnails/default-user-img-profile.jpg';
+    wrapper.setProps({ propsWithUserImgUrl });
+    expect(wrapper.find('img').length).toBe(1);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
